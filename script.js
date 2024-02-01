1c6acd6e7a932ed569db9d331184c6c1b890c3cd
JaiPremkumar <157562246+JaiPremkumar@users.noreply.github.com>
2024-02-01 12:27:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,420 @@
+function send(){
+    value1=document.getElementById("textipt").value;
+     element=document.createElement("div");
+
+    if(value1!==""){
+        text1=document.createTextNode(value1);
+        element.appendChild(text1);
+        mydiv=document.getElementById("mydiv1");
+        mydiv.appendChild(element);
+        element.id="mydiv"
+        element.style.width="10%";
+        element.style.height="auto";
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt").value="";
+    }
+}
+
+function send1(){
+    value2=document.getElementById("textipt1").value;
+     element=document.createElement("div");
+
+    if(value2!==""){
+        text2=document.createTextNode(value2);
+        element.appendChild(text2);
+        mydiv2=document.getElementById("mydiv2");
+        mydiv2.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt1").value="";
+    }
+}
+ 
+function send2(){
+    value3=document.getElementById("textipt2").value;
+     element=document.createElement("div");
+
+    if(value3!==""){
+        text3=document.createTextNode(value3);
+        element.appendChild(text3);
+        mydiv3=document.getElementById("mydiv3");
+        mydiv3.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt2").value="";
+    }
+}  
+
+
+function send3(){
+    value4=document.getElementById("textipt3").value;
+     element=document.createElement("div");
+
+    if(value4!==""){
+        text4=document.createTextNode(value4);
+        element.appendChild(text4);
+        mydiv4=document.getElementById("mydiv4");
+        mydiv4.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt3").value="";
+    }
+}
+
+
+function send4(){
+    value5=document.getElementById("textipt4").value;
+     element=document.createElement("div");
+
+    if(value5!==""){
+        text5=document.createTextNode(value5);
+        element.appendChild(text5);
+        mydiv5=document.getElementById("mydiv5");
+        mydiv5.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt4").value="";
+    }
+}
+
+function send5(){
+    value6=document.getElementById("textipt5").value;
+     element=document.createElement("div");
+
+    if(value6!==""){
+        text6=document.createTextNode(value6);
+        element.appendChild(text6);
+        mydiv6=document.getElementById("mydiv6");
+        mydiv6.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt5").value="";
+    }
+}
+
+function send6(){
+    value7=document.getElementById("textipt6").value;
+     element=document.createElement("div");
+
+    if(value7!==""){
+        text7=document.createTextNode(value7);
+        element.appendChild(text7);
+        mydiv7=document.getElementById("mydiv7");
+        mydiv7.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt6").value="";
+    }
+}
+function send7(){
+    value8=document.getElementById("textipt7").value;
+     element=document.createElement("div");
+
+    if(value8!==""){
+        text8=document.createTextNode(value8);
+        element.appendChild(text8);
+        mydiv8=document.getElementById("mydiv8");
+        mydiv8.appendChild(element);
+        element.style.width="10%";
+        element.style.height="auto"
+        element.style.backgroundColor="white";
+        element.style.color="black";
+        element.style.marginTop="3%";
+        element.style.borderRadius="50px";
+        element.style.marginRight="90%";
+        element.style.textAlign="center";
+        element.style.fontFamily= "Arial, Helvetica, sans-serif";
+        element.style.paddingTop="5px";
+        document.getElementById("textipt7").value="";
+    }
+}
+
+
+function person1(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv1.style.visibility="visible";
+    if(mydiv1.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv.style.display==="none"){
+        mydiv.style.display="block"
+    }
+}
+
+function person2(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv2.style.visibility="visible";
+    if(mydiv2.style.visibility==="visible"){
+        mydiv1.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv2.style.display==="none"){
+        mydiv2.style.display="block"
+    }
+}
+function person3(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv3.style.visibility="visible";
+    if(mydiv3.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv1.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv3.style.display==="none"){
+        mydiv3.style.display="block"
+    }
+}
+function person4(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv4.style.visibility="visible";
+    if(mydiv4.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv1.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv4.style.display==="none"){
+        mydiv4.style.display="block"
+    }
+}
+function person5(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv5.style.visibility="visible";
+    if(mydiv5.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv1.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv5.style.display==="none"){
+        mydiv5.style.display="block"
+    }
+}
+function person6(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv6.style.visibility="visible";
+    if(mydiv6.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv1.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv6.style.display==="none"){
+        mydiv6.style.display="block"
+    }
+}
+function person7(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv7.style.visibility="visible";
+    if(mydiv7.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv1.style.visibility="hidden";
+        mydiv8.style.visibility="hidden";
+    }
+    if(mydiv7.style.display==="none"){
+        mydiv7.style.display="block"
+    }
+}
+function person8(){
+    mydiv1=document.getElementById("mydiv1");
+    mydiv2=document.getElementById("mydiv2");
+    mydiv3=document.getElementById("mydiv3");
+    mydiv4=document.getElementById("mydiv4");
+    mydiv5=document.getElementById("mydiv5");
+    mydiv6=document.getElementById("mydiv6");
+    mydiv7=document.getElementById("mydiv7");
+    mydiv8=document.getElementById("mydiv8");
+    
+    mydiv8.style.visibility="visible";
+    if(mydiv8.style.visibility==="visible"){
+        mydiv2.style.visibility="hidden";
+        mydiv3.style.visibility="hidden";
+        mydiv4.style.visibility="hidden";
+        mydiv5.style.visibility="hidden";
+        mydiv6.style.visibility="hidden";
+        mydiv7.style.visibility="hidden";
+        mydiv1.style.visibility="hidden";
+    }
+    if(mydiv8.style.display==="none"){
+        mydiv8.style.display="block"
+    }
+}
+function close1(){
+    mydiv=document.getElementById("mydiv1");
+    mydiv.style.display="none"
+    
+}
+function close2(){
+    mydiv2=document.getElementById("mydiv2");
+    mydiv2.style.display="none"
+    
+}
+function close3(){
+    mydiv3=document.getElementById("mydiv3");
+    mydiv3.style.display="none"
+    
+}
+function close4(){
+    mydiv4=document.getElementById("mydiv4");
+    mydiv4.style.display="none"
+    
+}
+function close5(){
+    mydiv5=document.getElementById("mydiv5");
+    mydiv5.style.display="none"
+    
+}
+function close6(){
+    mydiv6=document.getElementById("mydiv6");
+    mydiv6.style.display="none"
+    
+}
+function close7(){
+    mydiv7=document.getElementById("mydiv7");
+    mydiv7.style.display="none"
+    
+}
+function close8(){
+    mydiv8=document.getElementById("mydiv8");
+    mydiv8.style.display="none"
+    
+}
